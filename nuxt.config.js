6467fea635c9d9a7d9adbb9cdf7cf805a1987ec7
Sam Littlefair <sam.littlefair@prismic.io>
2021-03-17 10:39:55 +0100

    Set target to static

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,7 @@ const smConfig = require("./sm.json");
 
 export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
+  target: "static",
   head: {
     title: "Loading...",
     meta: [
