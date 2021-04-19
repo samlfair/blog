d74c704caa231d008d5aa70b9865c43143dc6f68
Sam Littlefair <sam.littlefair@prismic.io>
2021-04-19 16:41:11 +0200

    Change rendering to server

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,7 @@ const smConfig = require("./sm.json");
 
 export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
-  target: "static",
+  target: "server",
   head: {
     title: "Loading...",
     meta: [
